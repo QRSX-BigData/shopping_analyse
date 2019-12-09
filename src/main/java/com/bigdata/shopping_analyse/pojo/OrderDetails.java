5e1546bafd996257cb60e043d6b5518b3dd82be1
Administrator <Administrator@USER-8E3A4G7ST9>
2019-12-09 16:40:45 +0800

    add orderdetails pojo

---
+++ src/main/java/com/bigdata/shopping_analyse/pojo/OrderDetails.java
@@ -0,0 +1,55 @@
+package com.bigdata.shopping_analyse.pojo;
+
+import java.util.Date;
+
+public class OrderDetails{
+	private int id;
+	private String order_id;
+	private int goods_id;
+	private int goods_num;
+	private double goods_price;
+	private Date create_time;
+	private Date update_time;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getOrder_id() {
+		return order_id;
+	}
+	public void setOrder_id(String order_id) {
+		this.order_id = order_id;
+	}
+	public int getGoods_id() {
+		return goods_id;
+	}
+	public void setGoods_id(int goods_id) {
+		this.goods_id = goods_id;
+	}
+	public int getGoods_num() {
+		return goods_num;
+	}
+	public void setGoods_num(int goods_num) {
+		this.goods_num = goods_num;
+	}
+	public double getGoods_price() {
+		return goods_price;
+	}
+	public void setGoods_price(double goods_price) {
+		this.goods_price = goods_price;
+	}
+	public Date getCreate_time() {
+		return create_time;
+	}
+	public void setCreate_time(Date create_time) {
+		this.create_time = create_time;
+	}
+	public Date getUpdate_time() {
+		return update_time;
+	}
+	public void setUpdate_time(Date update_time) {
+		this.update_time = update_time;
+	}
+}
