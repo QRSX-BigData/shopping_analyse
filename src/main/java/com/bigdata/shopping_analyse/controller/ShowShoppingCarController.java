bc0ac71454074eb44c0f626d7a78d315270f071c
Administrator <Administrator@USER-8E3A4G7ST9>
2019-12-10 14:21:04 +0800

    update

--- src/main/java/com/bigdata/shopping_analyse/controller/ShowShoppingCarController.java
+++ src/main/java/com/bigdata/shopping_analyse/controller/ShowShoppingCarController.java
@@ -1,34 +1,38 @@
 package com.bigdata.shopping_analyse.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.bigdata.shopping_analyse.mapper.ShoppingCarMapper;
+import com.bigdata.shopping_analyse.mapper.ShowShoppingCarMapper;
+import com.bigdata.shopping_analyse.pojo.Goods;
 import com.bigdata.shopping_analyse.pojo.ShoppingCar;
 
 @Controller
 public class ShowShoppingCarController {
-	@Autowired ShoppingCarMapper shoppingcarmapper;
+	@Autowired ShowShoppingCarMapper showshoppingcarmapper;
 
 	//查看当前用户的购物车里的商品
 	@RequestMapping("/ShowCarList") 
 	public String listShoppingCar(Model m,HttpServletRequest request) throws Exception {
 		HttpSession session = request.getSession();
 		
-		//获取session中的用户名
+		//获取session中的userid
 		int userid = (int) session.getAttribute("userid");
 		System.out.println("userid:"+userid);
 		
-		//将userid goodsid 插入购物车数据库
+		//根据userid 查询购物车内商品id ,根据商品id查询商品详情信息
 		ShoppingCar s =new ShoppingCar();
 		s.setUser_id(userid);
-		s.setGoods_id(1);
-		int num = shoppingcarmapper.insertgoods(s);
-		System.out.println(num);
+		List<Goods> goodslist = showshoppingcarmapper.selectGoodsInCar(s);
+		for(Goods g : goodslist) {
+			System.out.println(g);
+		}
 		
 	    // 移除session里的userid
 		// session.removeAttribute("userid");
