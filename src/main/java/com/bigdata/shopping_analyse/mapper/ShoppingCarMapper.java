6ac08a114372fad3edb8c4eef4a274771faee24c
Administrator <Administrator@USER-8E3A4G7ST9>
2019-12-10 08:14:08 +0800

    update

--- src/main/java/com/bigdata/shopping_analyse/mapper/ShoppingCarMapper.java
+++ src/main/java/com/bigdata/shopping_analyse/mapper/ShoppingCarMapper.java
@@ -7,6 +7,6 @@ import com.bigdata.shopping_analyse.pojo.ShoppingCar;
 
 @Mapper
 public interface ShoppingCarMapper {
-	@Insert("insert into shopping_car_info(user_id,goods_id) values(#{user_id},#{goods_id})")
+	@Insert("insert into shopping_car_info(user_id,goods_id,create_time,update_time) values(#{user_id},#{goods_id},now(),now())")
 	int insertgoods(ShoppingCar s);
 }
