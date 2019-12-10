9a8c9346eaeece770fed5c2734dabfd86b6ce5bd
Administrator <Administrator@USER-8E3A4G7ST9>
2019-12-10 14:30:56 +0800

    update

--- src/main/java/com/bigdata/shopping_analyse/mapper/ShowShoppingCarMapper.java
+++ src/main/java/com/bigdata/shopping_analyse/mapper/ShowShoppingCarMapper.java
@@ -2,7 +2,6 @@ package com.bigdata.shopping_analyse.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
