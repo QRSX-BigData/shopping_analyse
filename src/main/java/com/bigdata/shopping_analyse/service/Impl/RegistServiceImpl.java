6bc253037273e39a850bf4f903ba81ecf4a4935d
DearDolphin <1091877960@qq.com>
2019-12-11 16:05:54 +0800

    改个名

--- src/main/java/com/bigdata/shopping_analyse/service/Impl/RegistServiceImpl.java
+++ src/main/java/com/bigdata/shopping_analyse/service/Impl/RegistServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class registServiceImpl implements RegistService {
+public class RegistServiceImpl implements RegistService {
     @Autowired RegistMapper registMapper;
 
     @Override
