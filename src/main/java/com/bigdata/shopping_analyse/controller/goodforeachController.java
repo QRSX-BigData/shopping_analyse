0b2b2b2bd99f4d72053bade429f1c333c8650f0f
kingzuo <kingzuo@DESKTOP-AGF67N4>
2019-12-13 11:11:46 +0800

    update

--- src/main/java/com/bigdata/shopping_analyse/controller/goodforeachController.java
+++ src/main/java/com/bigdata/shopping_analyse/controller/goodforeachController.java
@@ -1,12 +1,9 @@
 package com.bigdata.shopping_analyse.controller;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import com.bigdata.shopping_analyse.mapper.GoodsMapper;
 import com.bigdata.shopping_analyse.pojo.Goods;
 
