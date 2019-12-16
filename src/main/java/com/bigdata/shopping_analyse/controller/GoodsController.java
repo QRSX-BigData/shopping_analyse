ae97f8c413c4b191c6df3386c536ac413ff759e3
kingz <kingz@DESKTOP-6GNN0CT>
2019-12-16 23:46:00 +0800

    update

--- src/main/java/com/bigdata/shopping_analyse/controller/GoodsController.java
+++ src/main/java/com/bigdata/shopping_analyse/controller/GoodsController.java
@@ -1,6 +1,9 @@
 package com.bigdata.shopping_analyse.controller;
 
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,11 +22,19 @@ public class GoodsController {
 		return "allgoods";
 	}
 	 
-//跳转到主页面
+	//跳转到主页面
 	@RequestMapping("/main") 
 	public String listmain(Model m) throws Exception {
 		List<Goods> shop=goodsmapper.findall();
 		m.addAttribute("shop", shop);
+		return "index";
+	}
+	//退出登录
+	@RequestMapping("/out")
+	public String out( HttpServletRequest request) throws Exception {
+		
+		// 把session中登录名移除
+		request.getSession().removeAttribute("userid");
 		return "index";
 	}
 }
