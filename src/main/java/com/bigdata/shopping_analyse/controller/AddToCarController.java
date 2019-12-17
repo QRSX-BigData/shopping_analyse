abafc4e521ab4594f9a71765e32ec16ab39c205a
kingz <kingz@DESKTOP-6GNN0CT>
2019-12-17 15:12:16 +0800

    update

--- src/main/java/com/bigdata/shopping_analyse/controller/AddToCarController.java
+++ src/main/java/com/bigdata/shopping_analyse/controller/AddToCarController.java
@@ -7,23 +7,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.bigdata.shopping_analyse.mapper.GoodsDetailsMapper;
 import com.bigdata.shopping_analyse.service.AddToCarService;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class AddToCarController {
 	@Autowired AddToCarService addtocarservice;
 	@Autowired GoodsDetailsMapper goodsdetailsmapper;
-
+	private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(AddToCarController.class);
 	// 添加商品到购物车
 	@RequestMapping("/addtocar")
 	public String listCategory(Model m, HttpServletRequest request, int id) throws Exception {
+		
+		
+		
 		//如果没有登录,跳转到登录页面
 		if(request.getSession().getAttribute("userid") == null) {
 			return "login";
 		}else {
 			addtocarservice.addgoods(id, (int) request.getSession().getAttribute("userid"));
 			m.addAttribute("shop", goodsdetailsmapper.selectgoodsdetails(id));
+			LOG.info("这是log4j2的日志测试，info级别");
+//			System.out.println("这是log4j2的日志测试，info级别");
 			return "shop1";
 		}
+		
 		
 	}
 }
