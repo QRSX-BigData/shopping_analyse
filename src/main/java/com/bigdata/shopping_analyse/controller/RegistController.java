0e41f73bc38c2936cca901339fa951173f875187
kingz <kingz@DESKTOP-6GNN0CT>
2019-12-18 20:49:13 +0800

    update

--- src/main/java/com/bigdata/shopping_analyse/controller/RegistController.java
+++ src/main/java/com/bigdata/shopping_analyse/controller/RegistController.java
@@ -10,23 +10,9 @@ import org.springframework.web.bind.annotation.*;
 //注册的Controller层
 public class RegistController {
     @Autowired RegistService registService;
-    private static final org.slf4j.Logger Log = org.slf4j.LoggerFactory.getLogger(AddToCarController.class);    //注册
     @RequestMapping("/register")
     public String add(@RequestBody Account s) throws Exception {
     	s.setPassword(DigestUtils.md5DigestAsHex(s.getPassword().getBytes()));
     	return registService.add(s);
-      /*  if(registService.get(s)==0){
-            //将密码设置为MD5加密
-            s.setPassword(DigestUtils.md5DigestAsHex(s.getPassword().getBytes()));
-            //添加到数据库中
-            if(registService.add(s)==1) {
-            	 Log.warn("0");
-            	 return "success";
-            }else{
-                return "false";
-            }
-        }else{
-            return "false";
-        }*/
     }
 }
